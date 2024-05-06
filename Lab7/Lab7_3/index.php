810caa2ed234a7163b2b8b3a23d8bf56f83a0ff7
s30274 <s30274@pjwstk.edu.pl>
2024-05-06 21:14:01 +0200

    Zadania z Lab7

--- Lab7/Lab7_3/index.php
+++ Lab7/Lab7_3/index.php
@@ -14,16 +14,3 @@ function zad3($a, $b, $c, $d){
 zad3(3, 13, 21, 30);
 
 ?>
-
-<html>
-<body>
-
-<form action="welcome.php" method="POST">
-    Name: <input type="text" name="name"><br>
-    E-mail: <input type="text" name="email"><br>
-    <input type="submit">
-</form>
-
-</body>
-</html>
-
